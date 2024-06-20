e669122896dde7a0e1283a5ce801bbb02bbe0549
juancmdev <juancmorenoj@gmail.com>
2024-06-19 19:19:51 -0500

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -9,21 +10,29 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(0, 2, 5); //x, y, z
+camera.position.set(-10, 30, 30); //x, y, z
+orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+
+const planetGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const plane = new THREE.Mesh(planetGeometry, planeMaterial);
+scene.add(plane);
 
 function animate(time) {
   box.rotation.x = time / 1000;
